6c4ff4f2f92fb351dccda9f71e648d912c10a41c
Misho's Mac <mishosmac@Misha-2.local>
2024-10-21 02:19:42 +0400

    first commit

---
+++ JPApexPredators17/JPApexPredators17App.swift
@@ -0,0 +1,17 @@
+//
+//  JPApexPredators17App.swift
+//  JPApexPredators17
+//
+//  Created by Misho's Mac on 21.10.24.
+//
+
+import SwiftUI
+
+@main
+struct JPApexPredators17App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
