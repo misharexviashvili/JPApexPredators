752dc78eb43fcf1999067b2278f163f24518eb73
Misho's Mac <mishosmac@Misha-2.local>
2024-10-21 18:48:18 +0400

    fix incorrect view modifiers location, add search with animation

--- JPApexPredators17/ContentView.swift
+++ JPApexPredators17/ContentView.swift
@@ -9,32 +9,56 @@ import SwiftUI
 
 struct ContentView: View {
     let predators = Predators()
-    
-    var body: some View {
-        List(predators.apexPredators) { predator in
-            HStack{
-                //                Dinosaur image
-                Image(predator.image)
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, height: 100)
-                    .shadow(color: .white, radius: 1)
-                VStack(alignment: .leading){
-                    //                    name
-                    Text(predator.name)
-                        .fontWeight(.bold)
-                    //                    type
-                    Text(predator.type.rawValue.capitalized)
-                        .font(.subheadline)
-                        .fontWeight(.semibold)
-                        .padding(.horizontal, 13)
-                        .padding(.vertical, 5)
-                        .background(predator.type.background)
-                        .clipShape(.capsule)
-                }
+    @State var searchText = ""
+    var filteredDinos : [ApexPredator] {
+        if searchText.isEmpty {
+            return predators.apexPredators
+        } else {
+            return predators.apexPredators.filter{
+                predator in
+                predator.name.localizedCaseInsensitiveContains(searchText)
             }
         }
-        .preferredColorScheme(/*@START_MENU_TOKEN@*/.dark/*@END_MENU_TOKEN@*/)
+    }
+    
+    var body: some View {
+        NavigationStack{
+            List(filteredDinos) { predator in
+                NavigationLink{
+                    Image(predator.image)
+                        .resizable()
+                        .scaledToFit()
+                    
+                } label : {
+                    HStack{
+                        //                Dinosaur image
+                        Image(predator.image)
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, height: 100)
+                            .shadow(color: .white, radius: 1)
+                        VStack(alignment: .leading){
+                            //                    name
+                            Text(predator.name)
+                                .fontWeight(.bold)
+                            //                    type
+                            Text(predator.type.rawValue.capitalized)
+                                .font(.subheadline)
+                                .fontWeight(.semibold)
+                                .padding(.horizontal, 13)
+                                .padding(.vertical, 5)
+                                .background(predator.type.background)
+                                .clipShape(.capsule)
+                        }
+                    }
+                }
+                
+            }
+            .navigationTitle("Apex Predators")
+            .searchable(text: $searchText)
+            .autocorrectionDisabled()
+            .animation(.default, value : searchText)
+        }.preferredColorScheme(/*@START_MENU_TOKEN@*/.dark/*@END_MENU_TOKEN@*/)
     }
 }
 
