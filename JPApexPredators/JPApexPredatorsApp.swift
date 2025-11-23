4e52b309f300779b6c2a6a5693cde0cbe64a1997
Mikhaeli <Mihaelirekhviashvili@gmail.com>
2025-11-23 13:21:30 +0400

    Initial Commit

---
+++ JPApexPredators/JPApexPredatorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  JPApexPredatorsApp.swift
+//  JPApexPredators
+//
+//  Created by Credo on 11/23/25.
+//
+
+import SwiftUI
+
+@main
+struct JPApexPredatorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
