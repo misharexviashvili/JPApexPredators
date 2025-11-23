e21b0fc83abe17a05bcd248a8caefdcacde411c4
Mikhaeli <Mihaelirekhviashvili@gmail.com>
2025-11-23 19:29:10 +0400

    sorting and search

--- JPApexPredators/ContentView.swift
+++ JPApexPredators/ContentView.swift
@@ -10,36 +10,31 @@ import SwiftUI
 struct ContentView: View {
     let predators = Predators()
     @State var searchText = ""
-    var filteredDinos : [ApexPredator]{
-        if searchText.isEmpty {
-            return predators.apexPredators
-        } else {
-            return predators.apexPredators.filter {
-                predator in
-                predator.name.localizedCaseInsensitiveContains(searchText)
-            }
-        }
+    @State var alphabetical = false
+    var filteredDinos: [ApexPredator] {
+        predators.sort(by: alphabetical)
+        return predators.search(for: searchText)
     }
-    
+
     var body: some View {
-        NavigationStack{
-            List(filteredDinos){
+        NavigationStack {
+            List(filteredDinos) {
                 predator in
                 NavigationLink {
                     Image(predator.image)
                         .resizable()
                         .scaledToFit()
-                } label : {
-                    HStack{
+                } label: {
+                    HStack {
                         Image(predator.image)
                             .resizable()
                             .scaledToFit()
                             .frame(width: 100, height: 100)
                             .shadow(color: .white, radius: 1)
-                        VStack(alignment: .leading){
+                        VStack(alignment: .leading) {
                             Text(predator.name)
                                 .fontWeight(.bold)
-                            
+
                             Text(predator.type.rawValue.capitalized)
                                 .font(.subheadline)
                                 .fontWeight(.semibold)
@@ -47,18 +42,32 @@ struct ContentView: View {
                                 .padding(.vertical, 5)
                                 .background(predator.type.background)
                                 .clipShape(.capsule)
-                            
+
                         }
                     }
                 }
-               
+
             }
             .navigationTitle("Apex Predators")
             .searchable(text: $searchText)
             .autocorrectionDisabled()
             .animation(.default, value: searchText)
+            .toolbar {
+                ToolbarItem(placement: .topBarLeading) {
+                    Button {
+                        withAnimation {
+                            alphabetical.toggle()
+                        }
+                    } label: {
+                        Image(systemName: alphabetical ? "film" : "textformat")
+                            .symbolEffect(.bounce, value: alphabetical)
+                    }
+                }
+            }
+
         }
         .preferredColorScheme(.dark)
+
     }
 }
 
--- JPApexPredators/Predators.swift
+++ JPApexPredators/Predators.swift
@@ -25,6 +25,27 @@ class Predators {
              print("Error decoding JSON data \(error)")
             }
         }
-            
+    }
+    
+    func search (for searchTerm: String)-> [ApexPredator] {
+        if searchTerm.isEmpty {
+            return apexPredators
+        } else {
+            return apexPredators.filter {
+                predator in
+                predator.name.localizedCaseInsensitiveContains(searchTerm)
+            }
+        }
+    }
+    
+    func sort (by alphabetical: Bool){
+        apexPredators.sort{
+            predator1, predator2 in
+            if alphabetical {
+                predator1.name < predator2.name
+            } else {
+                predator1.id < predator2.id
+            }
+        }
     }
 }
